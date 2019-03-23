9c11394dc705e16418f6fcfe3f75e7987df1c9ab
Ethan Kennerly <ethankennerly@gmail.com>
2019-03-23 11:52:24 -0700

    Phase Rotation Tween: Rotate more than half circle

--- PorpoiseOfClapping/Assets/Tiny/PorpoiseOfClapping/Scripts/PhaseRotationTweenSystem.ts
+++ PorpoiseOfClapping/Assets/Tiny/PorpoiseOfClapping/Scripts/PhaseRotationTweenSystem.ts
@@ -1,5 +1,18 @@
 namespace game {
     export class PhaseRotationTweenSystem extends ut.ComponentSystem {
+        minDuration:number = 0.001;
+
+        // Binds `this` to this class.
+        // Otherwise, binds `this` to browser `window`.
+        // Only allocates memory once.
+        // Otherwise, allocates memory per call.
+        // https://github.com/Microsoft/TypeScript/wiki/'this'-in-TypeScript#use-instance-functions
+        startTweenRotationInstance = (entity:ut.Entity,
+                rotationTweener:game.RotationTweener,
+                transformRotation:ut.Core2D.TransformLocalRotation) => {
+            this.startTweenRotation(entity, rotationTweener, transformRotation);
+        };
+
         OnUpdate():void {
             let phaseConfig = this.world.getConfigData(game.PhaseConfig);
             let phaseIncreased:boolean = phaseConfig.changed && phaseConfig.phase > 0;
@@ -9,22 +22,44 @@ namespace game {
 
             this.world.forEach(
                 [ut.Entity, game.RotationTweener, ut.Core2D.TransformLocalRotation],
-                (entity, rotationTweener, transformRotation) => {
-                    this.tweenRotationShort(entity, transformRotation, rotationTweener.endRadians, rotationTweener.duration);
-                }
+                this.startTweenRotationInstance
             );
+        }
+
+        // Able to rotation beyond half a circle.
+        //
+        // Sets speed of tween rotation and resets tween time to 0.
+        // Depends on TweenRotationSystem to update the rotation.
+        startTweenRotation(entity:ut.Entity,
+                rotationTweener:game.RotationTweener,
+                transformRotation:ut.Core2D.TransformLocalRotation):void {
+            if (rotationTweener.endRadians == 0) {
+                return;
+            }
+
+            let startRotation = new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), 0);
+            transformRotation.rotation = startRotation;
+            let duration:number = rotationTweener.duration;
+            if (duration <= this.minDuration) {
+                duration = this.minDuration;
+            }
+
+            if (!this.world.hasComponent(entity, game.TweenRotation)) {
+                this.world.addComponent(entity, game.TweenRotation);
+            }
+            let tweenRotation:game.TweenRotation = this.world.getComponentData(entity, game.TweenRotation);
+            tweenRotation.speed = rotationTweener.endRadians / duration;
+            tweenRotation.time = 0;
+            this.world.setComponentData(entity, tweenRotation);
         }
 
         // Tween rotation, by shortest distance, never more than half circle.
         // To rotate nearly a full circle, nest one rotator inside another.
         // Copied from TinySamples DinosaurService.ts
         //
-        // An alternative is rotating every frame, as in HelloCube sample in Unity.Entities C# sample:
-        //
-        //      rotation.Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(math.up(), rotSpeed.Value * dT));
+        // An alternative is rotating every frame, as invoked by `startTweenRotation`.
         tweenRotationShort(entity:ut.Entity, transformRotation:ut.Core2D.TransformLocalRotation,
-                endRadians:number, duration:number):void
-        {
+                endRadians:number, duration:number):void {
             let startRotation = new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), 0);
             let endRotation = new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), endRadians);
             transformRotation.rotation = startRotation;
--- PorpoiseOfClapping/Assets/Tiny/PorpoiseOfClapping/Scripts/TweenRotationSystem.ts
+++ PorpoiseOfClapping/Assets/Tiny/PorpoiseOfClapping/Scripts/TweenRotationSystem.ts
@@ -1,15 +1,28 @@
 namespace game {
+    // Depends on RotationTweener configuring and adding TweenRotation.
     export class TweenRotationSystem extends ut.ComponentSystem {
 
         deltaTime:number;
         rotationStep:Quaternion = new Quaternion();
         zAxis:Vector3 = new Vector3(0, 0, 1);
 
+        // Binds `this` to this class.
+        // Otherwise, binds `this` to browser `window`.
+        // Only allocates memory once.
+        // Otherwise, allocates memory per call.
+        // https://github.com/Microsoft/TypeScript/wiki/'this'-in-TypeScript#use-instance-functions
+        updateRotationInstance = (entity:ut.Entity,
+                rotationTweener:game.RotationTweener,
+                tweenRotation:game.TweenRotation,
+                transformRotation:ut.Core2D.TransformLocalRotation) => {
+            this.updateRotation(entity, rotationTweener, tweenRotation, transformRotation);
+        };
+
         OnUpdate():void {
             this.deltaTime = this.scheduler.deltaTime();
             this.world.forEach(
                 [ut.Entity, game.RotationTweener, game.TweenRotation, ut.Core2D.TransformLocalRotation],
-                this.updateRotation);
+                this.updateRotationInstance);
         }
 
         // Rotating every frame, as in HelloCube sample in Unity.Entities C# sample:
@@ -29,6 +42,7 @@ namespace game {
                 this.world.removeComponent(entity, game.TweenRotation);
                 return;
             }
+            this.world.setComponentData(entity, tweenRotation);
 
             let stepAngle:number = tweenRotation.speed * this.deltaTime;
             this.rotationStep.setFromAxisAngle(this.zAxis, stepAngle);
